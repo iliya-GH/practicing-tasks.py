62b2c45108ec1ce986811190751c207e9aa04baf
iliya-GH <iliyaghi13@gmail.com>
2025-02-24 22:23:45 +0330

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,23 @@
+#question 1
+x = int(input("R: "))
+s = x ** 2 * 3.14
+p = x * 2 * 3.14
+print("s:", int(s))
+print("p:", int(p))
+#question 2
+y = float(input("x: "))
+t2 = y ** 2
+t3 = y ** 3
+print("t2:", t2)
+print("t3:", t3)
+#question 3
+z = float(input("a: "))
+w = float(input("b: "))
+i = z ** y
+print("i:", i)
+#question 4
+u = input("t:")
+o = input("j:")
+p = input("K:")
+d = u + o + p
+print(int(int(d) / 3))
